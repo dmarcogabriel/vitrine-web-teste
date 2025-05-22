eee9d53fa99a39843c3e6c3acf37c344b2f2a741
Gabriel Marco <lino.dmarco@gmail.com>
2025-05-22 11:04:46 -0300

    feat(web): add back button on header

--- web/src/components/ui/header.jsx
+++ web/src/components/ui/header.jsx
@@ -1,9 +1,28 @@
+'use client';
+
+import {ArrowLeft} from 'lucide-react';
 import Link from 'next/link';
+import {usePathname, useRouter} from 'next/navigation';
 
 export function Header() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isRootScreen = pathname === '/';
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
   return (
-    <nav className="flex justify-center p-4 shadow-sm">
-      <Link href="/">
+    <nav className="flex justify-center p-4 shadow-sm gap-2">
+      {!isRootScreen ? (
+        <button type="button" onClick={handleGoBack}>
+          <ArrowLeft />
+        </button>
+      ) : null}
+
+      <Link href="/" className="flex-1 text-center">
         <p className="text-2xl font-bold">Vitrine de Produtos</p>
       </Link>
     </nav>
